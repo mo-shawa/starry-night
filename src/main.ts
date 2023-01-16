82351cd32991179818a115a8f08f6f55a1d6d079
moshawa <moshawa0@gmail.com>
2023-01-15 19:32:49 -0500

    version without rotation, just zoom out to animated galaxy

--- src/main.ts
+++ src/main.ts
@@ -57,15 +57,15 @@ renderer.domElement.classList.add("webgl")
 document.getElementById("app")?.prepend(renderer.domElement)
 
 const parameters = {
-	count: 10000,
+	count: 180000,
 	size: 0.005,
-	radius: 0.5,
+	radius: 1.5,
 	branches: 6,
 	spin: 1,
-	randomness: 0.6,
+	randomness: 0.9,
 	insideColor: "#ffffff",
 	outsideColor: "#35ffee",
-	swirlRatio: 400,
+	swirlRatio: 800,
 }
 
 /**
@@ -143,8 +143,7 @@ const tick = () => {
 	const elapsedTime = clock.getElapsedTime()
 
 	pointsMaterial.uniforms.uTime.value =
-		(500 + elapsedTime) / parameters.swirlRatio
-	camera.lookAt(points.position)
+		(100 + elapsedTime) / parameters.swirlRatio
 
 	renderer.render(scene, camera)
 
@@ -170,10 +169,11 @@ const galaxyTimeline = gsap.timeline({
 })
 
 galaxyTimeline
-	.to(points.rotation, { z: 0.3, ease: "expo.out" })
+	// .to(points.rotation, { z: 0.3, ease: "expo.out" }, 0)
 	.from(
 		pointsMaterial.uniforms.uSize,
 		{ value: 0 * renderer.getPixelRatio() },
 		0
 	)
-	.to(parameters, { swirlRatio: 50 }, 0)
+	.to(parameters, { swirlRatio: 2 }, 0)
+	.to(camera.position, { y: 2, x: -1 }, 0)
